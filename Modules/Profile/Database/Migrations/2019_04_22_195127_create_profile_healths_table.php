987711b8b8f412ca65211889fb2d0e68f0efbf2c
Abu Saleem <abusaleem>
2019-04-22 21:08:02 +0100

    set the profile health migration

--- Modules/Profile/Database/Migrations/2019_04_22_195127_create_profile_healths_table.php
+++ Modules/Profile/Database/Migrations/2019_04_22_195127_create_profile_healths_table.php
@@ -15,7 +15,38 @@ class CreateProfileHealthsTable extends Migration
     {
         Schema::create('profile_healths', function (Blueprint $table) {
             $table->increments('id');
-
+            $table->integer('profile_id')
+            ->unsigned()
+            ->nullable()
+            ->foreign()
+            ->refernces('id')
+            ->on('profiles')
+            ->delete('restrict')
+            ->update('cascade');
+            $table->integer('desease_id')
+            ->unsigned()
+            ->nullable()
+            ->foreign()
+            ->refernces('id')
+            ->on('deseases')
+            ->delete('restrict')
+            ->update('cascade');
+            $table->integer('genotype_id')
+            ->unsigned()
+            ->nullable()
+            ->foreign()
+            ->refernces('id')
+            ->on('genotypes')
+            ->delete('restrict')
+            ->update('cascade');
+            $table->integer('blood_group_id')
+            ->unsigned()
+            ->nullable()
+            ->foreign()
+            ->refernces('id')
+            ->on('blood_groups')
+            ->delete('restrict')
+            ->update('cascade');
             $table->timestamps();
         });
     }
