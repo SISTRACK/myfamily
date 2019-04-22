d9b152cbcd3893fc339745a80c392615b4ce260d
Abu Saleem <abusaleem>
2019-04-22 14:54:29 +0100

    set experience profile eloquent relation ship

--- Modules/Profile/Entities/Experience.php
+++ Modules/Profile/Entities/Experience.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Experience extends Model
 {
-    protected $fillable = [];
+    protected $guarded = [];
+
+    public function profileExperiences()
+    {
+    	return $this->hasMany(ProfileExperience::class);
+    }
 }
--- Modules/Profile/Entities/ProfileExperience.php
+++ Modules/Profile/Entities/ProfileExperience.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProfileExperience extends Model
 {
-    protected $fillable = [];
+    protected $guarded = [];
+
+    public function profile()
+    {
+        return $this->belongsTo(Profile::class);
+    }
+
+    public function experience()
+    {
+        return $this->belongsTo(Experience::class);
+    }
 }
