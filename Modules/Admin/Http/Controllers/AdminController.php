5dce1849d2194af6052966ae286ce64739deb54f
Abu Saleem <abusaleem>
2019-06-24 19:39:49 +0100

    create admin login controller

--- Modules/Admin/Http/Controllers/AdminController.php
+++ Modules/Admin/Http/Controllers/AdminController.php
@@ -9,65 +9,98 @@ use Modules\Core\Http\Controllers\BaseController;
 
 class AdminController extends BaseController
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:admin',['only' => 'index','edit']);
+    }
+
     /**
      * Display a listing of the resource.
-     * @return Response
+     *
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('admin::index');
+        return view('admin.dashboard');
     }
 
     /**
      * Show the form for creating a new resource.
-     * @return Response
+     *
+     * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        return view('admin::create');
+        return view('admin.auth.register');
     }
 
     /**
      * Store a newly created resource in storage.
-     * @param  Request $request
-     * @return Response
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        // validate the data
+        $this->validate($request, [
+          'name'          => 'required',
+          'email'         => 'required',
+          'password'      => 'required'
+        ]);
+        // store in the database
+        $admins = new Admin;
+        $admins->name = $request->name;
+        $admins->email = $request->email;
+        $admins->password=bcrypt($request->password);
+        $admins->save();
+        return redirect()->route('admin.auth.login');
     }
 
     /**
-     * Show the specified resource.
-     * @return Response
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show($id)
     {
-        return view('admin::show');
+        //
     }
 
     /**
      * Show the form for editing the specified resource.
-     * @return Response
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function edit()
+    public function edit($id)
     {
-        return view('admin::edit');
+        //
     }
 
     /**
      * Update the specified resource in storage.
-     * @param  Request $request
-     * @return Response
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
+    public function update(Request $request, $id)
     {
+        //
     }
 
     /**
      * Remove the specified resource from storage.
-     * @return Response
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
      */
-    public function destroy()
+    public function destroy($id)
     {
+        //
     }
+    
 }
