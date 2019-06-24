9948049b67450c103e3221f7e6fcc62f101d52d6
Abu Saleem <abusaleem>
2019-06-24 19:48:06 +0100

    configure the rout file

--- Modules/Admin/Routes/web.php
+++ Modules/Admin/Routes/web.php
@@ -14,3 +14,12 @@
 Route::prefix('admin')->group(function() {
     Route::get('/', 'AdminController@index');
 });
+Route::prefix('admin')->group(function () {
+  Route::get('/', 'AdminController@index')->name('admin.dashboard');
+  Route::get('dashboard', 'AdminController@index')->name('admin.dashboard');
+  Route::get('register', 'AdminController@create')->name('admin.register');
+  Route::post('register', 'AdminController@store')->name('admin.register.store');
+  Route::get('login', 'Auth\LoginController@login')->name('admin.auth.login');
+  Route::post('login', 'Auth\LoginController@loginAdmin')->name('admin.auth.loginAdmin');
+  Route::post('logout', 'Auth\LoginController@logout')->name('admin.auth.logout');
+});
