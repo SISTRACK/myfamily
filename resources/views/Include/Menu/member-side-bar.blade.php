a432bfd0d4640d03e148867a46dc63f7558b7f75
Abu Saleem <abusaleem>
2019-07-19 19:04:22 +0100

    fixed husband divorec restriction error

--- resources/views/Include/Menu/member-side-bar.blade.php
+++ resources/views/Include/Menu/member-side-bar.blade.php
@@ -3,7 +3,7 @@
         <ul class="list-unstyled">
             <li><a href="{{route('family.birth.create',[profile()->thisProfileFamily()->name])}}"><i class="mdi mdi-baby"></i> <span>Birth</span></a></li>
             <li><a href="{{route('family.marriage.create',[profile()->thisProfileFamily()->name])}}">Marriage</a></li>
-            @if(profile()->husband && canDivorce)
+            @if(profile()->husband && canDivorce())
             <li><a href="{{route('family.divorce.create',[profile()->thisProfileFamily()->name])}}">Divorce</a></li>
             @endif
             <li><a href="{{route('family.death.create',[profile()->thisProfileFamily()->name])}}">Death</a></li>
