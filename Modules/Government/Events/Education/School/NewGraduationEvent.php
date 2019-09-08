b0f0becc2e10a9e283d0099820795d2ea92058a6
Abu Saleem <abusaleem>
2019-09-08 19:00:30 +0100

    create school statistics event for the government

---
+++ Modules/Government/Events/Education/School/NewGraduationEvent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Government\Events\Education\School;
+
+use Illuminate\Queue\SerializesModels;
+
+class NewGraduationEvent
+{
+    use SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+}
